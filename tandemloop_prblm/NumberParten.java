911e4a307356d726fd4375e195b090a804b970c1
Shubharanshu mohanty <84987842+subhransu-mohanty@users.noreply.github.com>
2021-07-14 19:04:57 +0530

    Update NumberParten.java

--- tandemloop_prblm/NumberParten.java
+++ tandemloop_prblm/NumberParten.java
@@ -4,9 +4,8 @@ public class NumberParten {
 
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
-    System.out.println("Enter the number of rows to print the pattern ");
+    System.out.println("Enter the number");
     int rows = sc.nextInt();
-    System.out.println("* Printing the pattern... *");
     for (int i = 1; i <= rows; i++) {
       for (int j = 1; j <= i; j++) {
         System.out.print(j + " ");
